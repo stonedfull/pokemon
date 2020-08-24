17584d8ebc3d3c86f4aa4b3d4d21d278464e686b
viktorenko <vityablokhin7@gmail.com>
2020-08-24 14:44:11 +0300

    HomeWork 4

--- main.js
+++ main.js
@@ -2,62 +2,64 @@ const btn = document.querySelector('#btn-kick');
 
 const character = {
     name: 'Pikachu',
-    defaultHp: 100,
+    defaultHp: 200,
     damageHp: 100,
+    damage: 20,
+    isEnemy: false,
     elHp: document.querySelector('#health-character'),
     elPrograssbar: document.querySelector('#progressbar-character'),
+    render: renderHp,
+    hit: doHit,
 };
 
 const enemy = {
     name: 'Charmander',
     defaultHp: 100,
     damageHp: 100,
+    damage: 20,
+    isEnemy: true,
     elHp: document.querySelector('#health-enemy'),
     elPrograssbar: document.querySelector('#progressbar-enemy'),
+    render: renderHp,
+    hit: doHit,
 };
 
-btn.addEventListener('click', function () {
+function renderHp() {
+    this.elHp.textContent = this.damageHp + ' / ' + this.defaultHp;
+    this.elPrograssbar.style.width = this.damageHp / (this.defaultHp / 100) + '%';
+};
+
+function doHit() {
+    function random(max) {
+        let min = max - 8;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    let damage = random(this.damage), opponent;
+    this.isEnemy ? opponent = character : opponent = enemy;
+    const message = () => alert('Бедный ' + opponent.name + ' проиграл бой!');
+
+    if (opponent.damageHp <= damage) {
+        opponent.damageHp = 0;
+        btn.disabled = true;
+        setTimeout(message, 500);
+    } else {
+        opponent.damageHp -= damage;
+    }
+
+    opponent.render();
+}
+
+btn.addEventListener('click', () => {
     console.log('Kick!');
-    changeHp(20, character);
-    changeHp(20, enemy);
+    enemy.hit();
+    character.hit();
 })
 
 function init() {
     console.log('Start Game!');
-    renderHp(character);
-    renderHp(enemy);
+    character.render();
+    enemy.render();
 };
-
-function renderHp(person) {
-    renderHpLife(person);
-    renderPrograssbarHp(person);
-};
-
-function renderHpLife(person) {
-    person.elHp.innerText = person.damageHp + ' / ' + person.defaultHp;
-};
-
-function renderPrograssbarHp(person) {
-    person.elPrograssbar.style.width = person.damageHp + '%';
-};
-
-function changeHp(count, person) {
-    count = random(count);
-
-    if (person.damageHp <= count) {
-        person.damageHp = 0;
-        alert('Бедный ' + person.name + ' проиграл бой!');
-        btn.disabled = true;
-
-    } else {
-        person.damageHp -= count;
-    }
-
-    renderHp(person);
-}
-
-function random(num) {
-    return Math.ceil(Math.random() * num);
-}
 
 init();
